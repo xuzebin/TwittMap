170e9384fc839d62f627f31eecba9f8edbf5c0a5
xuzebin <zebinxu7@gmail.com>
2017-03-18 02:37:48 -0400

    temporarily not using static files

--- twittmap/settings.py
+++ twittmap/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -28,7 +27,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'twittmap-env.f2xpr2cnnc.us-east-1.elasticbeanstalk.com',
     '127.0.0.1',
-    'localhost',
+    'localhost'
 ]
 
 
